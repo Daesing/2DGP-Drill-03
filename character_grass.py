f666cdc4ad4b454b6e68c27b20e883ed7ff683f7
Daesing <peterpauler77@naver.com>
2024-09-24 11:49:55 +0900

    뼈대 완성 무한루프

---
+++ character_grass.py
@@ -0,0 +1,25 @@
+from pico2d import *
+
+open_canvas()
+
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+# fill here
+
+def run_rect():
+    print('rect')
+    pass
+def run_circle():
+    print('circle')
+    pass
+
+while True:
+    run_rect()
+    run_circle()
+
+
+
+ 
+close_canvas()
