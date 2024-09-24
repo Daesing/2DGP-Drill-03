b30a2ba26f87375271cbdeafe41af0e9e07f0492
Daesing <peterpauler77@naver.com>
2024-09-24 11:56:16 +0900

    캐릭터 화면 출력 구현

--- character_grass.py
+++ character_grass.py
@@ -13,11 +13,24 @@ def run_rect():
     pass
 def run_circle():
     print('circle')
+
+    clear_canvas_now()
+    character.draw_now(400,300)
+    delay(0.1)
     pass
 
 while True:
     run_rect()
     run_circle()
+    break
+
+
+while True:
+    user_input=input("Enter q to quit: ")
+    if user_input.lower()=="q":
+        break
+        
+        
 
 
 
